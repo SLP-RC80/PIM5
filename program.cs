c49de0e8c759fb4344ad1a80cac0ddd1231e332c
SLPRC <slp.rc.facu@gmail.com>
2026-03-09 07:37:43 -0300

    Configure server URL and add root endpoint
    Updated the application to listen on all network interfaces and added a root endpoint that returns a message.

--- program.cs
+++ program.cs
@@ -2,26 +2,24 @@ using PIMEventosTI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// registrar controllers
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 builder.Services.AddControllers();
 
-// registrar services
 builder.Services.AddSingleton<EventoService>();
 builder.Services.AddSingleton<InscricaoService>();
 builder.Services.AddSingleton<CertificadoService>();
 
-// habilitar documentação da API
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger (documentação automática da API)
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
 app.MapControllers();
+
+app.MapGet("/", () => "API PIMEventosTI rodando");
 
 app.Run();
